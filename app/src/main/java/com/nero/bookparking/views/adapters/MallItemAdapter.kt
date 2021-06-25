052c66ea6a5848673109af40eba26b6564c040d3
Chaithanya Gollapalli <chaitanyagcg.01@gmail.com>
2021-06-26 00:25:01 +0530

    login and list done

---
+++ app/src/main/java/com/nero/bookparking/views/adapters/MallItemAdapter.kt
@@ -0,0 +1,27 @@
+package com.nero.bookparking.views.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nero.bookparking.R
+import com.nero.bookparking.dto.parkingDTO.MallItem
+import com.nero.bookparking.views.OnItemClickListener
+import com.nero.bookparking.views.viewHolders.MallItemViewHolder
+
+class MallItemAdapter(private val list: List<MallItem>,private val itemClickListener: OnItemClickListener) :
+    RecyclerView.Adapter<MallItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MallItemViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.mall_item_layout, parent, false)
+        return MallItemViewHolder(view,itemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: MallItemViewHolder, position: Int) {
+        val mallItem = list[position]
+        holder.setData(mallItem)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
