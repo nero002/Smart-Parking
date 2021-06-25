651e4c7b2ff0a0f3e5de722875365947d325efb4
Dheeraj Gupta <38027375+nero002@users.noreply.github.com>
2021-06-25 16:36:23 +0530

    Init

---
+++ app/src/main/java/com/nero/bookparking/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.nero.bookparking
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
