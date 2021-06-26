1c00f0395cfcbca061382f57d8b7e3cd38ea3074
Dheeraj Gupta <38027375+nero002@users.noreply.github.com>
2021-06-26 17:08:33 +0530

    slotBooking

--- app/src/main/java/com/nero/bookparking/ui/slotBooking/SlotBookingFragment.kt
+++ app/src/main/java/com/nero/bookparking/ui/slotBooking/SlotBookingFragment.kt
@@ -1,27 +1,92 @@
 package com.nero.bookparking.ui.slotBooking
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nero.bookparking.R
+import com.nero.bookparking.databinding.FragmentSlotBookingBinding
 
 
 class SlotBookingFragment : Fragment() {
+    private var _binding: FragmentSlotBookingBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slot_booking, container, false)
+        _binding = FragmentSlotBookingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+
+        binding.apply {
+
+            cv13hrs.setOnClickListener {
+                cv13hrs.setCardBackgroundColor(resources.getColor(R.color.app_red))
+                cv34Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv57Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv7Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                tvCost.text = getString(R.string.rs20)
+                tvHiddenText.visibility = View.GONE
+
+            }
+
+            cv34Hrs.setOnClickListener {
+
+
+                cv13hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv34Hrs.setCardBackgroundColor(resources.getColor(R.color.app_red))
+                cv57Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv7Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                tvCost.text = getString(R.string.rs40)
+                tvHiddenText.visibility = View.GONE
+
+            }
+
+
+            cv57Hrs.setOnClickListener {
+
+
+                cv13hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv34Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv57Hrs.setCardBackgroundColor(resources.getColor(R.color.app_red))
+                cv7Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                tvCost.text = getString(R.string.rs100)
+                tvHiddenText.visibility = View.GONE
+
+            }
+            cv7Hrs.setOnClickListener {
+
+
+                cv13hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv34Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv57Hrs.setCardBackgroundColor(resources.getColor(R.color.white))
+                cv7Hrs.setCardBackgroundColor(resources.getColor(R.color.app_red))
+                tvCost.text = getString(R.string.rs200)
+                tvHiddenText.visibility = View.VISIBLE
+
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
+
+
